c5bf0469401b0a14a9cb421d6713406c84650098
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2024-04-04 23:53:48 +0530

    Added DayMonth Data and Weather logo

--- js/main.js
+++ js/main.js
@@ -7,13 +7,43 @@ async function checkWeather() {
 
     console.log(data);
 
-    // document.querySelector()
+    const today = new Date();
+    
+    
+    function addDays(date,  days) {
+        const result = new Date(date);
+        result.setDate(result.getDate() + days);
+        return result;
+    }
+
+    const sevenDays = [];
+    for (let i = 0; i < 7; i++) {
+        const nextDate = addDays(today, i);
+        const currDay = nextDate.toLocaleString('en-us', { weekday: 'short'});
+        const currMonth = nextDate.toLocaleString('en-us', { month: 'long'});
+        const currDate = nextDate.getDate();
+        const formattedDate = `${currDay} ${currMonth} ${currDate}`;
+        sevenDays.push(formattedDate);
+    }
+
+    // sevenDays.forEach(item => {
+    //     console.log(item);
+    // });
+
+    function setData() { 
+        for(let i = 0; i < 7; i++) {
+            const valPass = `.timeData-${i}`;
+            // console.log(valPass);
+            const timeData = document.querySelector(valPass);
+            timeData.innerHTML = sevenDays[i];
+        }
+    }
+    setData();
 }
 
 checkWeather();
 
 const appear = document.querySelector(".appear");
-console.log(appear);
 appear.style.display = "none";
 
 const selectElement = document.getElementById('drop-down');
