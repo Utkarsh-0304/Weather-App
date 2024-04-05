ddbede8ed1b58aa2077f495d486a0ff8ea33f6f2
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2024-04-05 23:53:07 +0530

    Updated images a/c API

--- js/main.js
+++ js/main.js
@@ -26,10 +26,6 @@ async function checkWeather() {
         sevenDays.push(formattedDate);
     }
 
-    // sevenDays.forEach(item => {
-    //     console.log(item);
-    // });
-
     function setData() { 
         for(let i = 0; i < 7; i++) {
             const valPass = `.timeData-${i}`;
@@ -39,18 +35,84 @@ async function checkWeather() {
         }
     }
     setData();
+
+    const appear = document.querySelector(".appear");
+    appear.style.display = "none";
+
+    const selectElement = document.getElementById('drop-down');
+
+        
+    selectElement.addEventListener('input', function(event) {
+        const selectValue = selectElement.value;
+        if(event.target.value !== 0) {
+            appear.style.display = "block"
+        }
+    });
+
+    const img = document.querySelectorAll("img");
+    for (let i = 0; i < 7; i++) {
+
+        
+        if(data.dataseries[i].weather === "clearday" || data.dataseries[i].weather === "clearnight") {
+            img[i].src = "images/clear.png";
+        }
+
+        else if(data.dataseries[i].weather === "pcloudyday" || data.dataseries[i].weather === "pcloudynight") {
+            img[i].src = "images/pcloudy.png";
+        }
+
+        else if(data.dataseries[i].weather === "mcloudyday" || data.dataseries[i].weather === "mcloudynight") {
+            img[i].src = "images/mcloudy.png";
+        }
+
+        else if(data.dataseries[i].weather === "cloudyday" || data.dataseries[i].weather === "cloudynight") {
+            img[i].src = "images/cloudy.png";
+        }
+
+        else if(data.dataseries[i].weather === "humidday" || data.dataseries[i].weather === "humidnight") {
+            img[i].src = "images/humid.png";
+        }
+
+        else if(data.dataseries[i].weather === "lightrainday" || data.dataseries[i].weather === "lightrainnight") {
+            img[i].src = "images/lightrain.png";
+        }
+
+        else if(data.dataseries[i].weather === "oshowerday" || data.dataseries[i].weather === "oshowernight") {
+            img[i].src = "images/oshower.png";
+        }
+
+        else if(data.dataseries[i].weather === "ishowerday" || data.dataseries[i].weather === "ishowernight") {
+            img[i].src = "images/ishower.png";
+        }
+
+        else if(data.dataseries[i].weather === "lightsnowday" || data.dataseries[i].weather === "lightsnownight") {
+            img[i].src = "images/lightsnow.png";
+        }
+
+        else if(data.dataseries[i].weather === "rainday" || data.dataseries[i].weather === "rainnight") {
+            img[i].src = "images/rain.png";
+        }
+
+        else if(data.dataseries[i].weather === "snowday" || data.dataseries[i].weather === "snownight") {
+            img[i].src = "images/snow.png";
+        }
+
+        else if(data.dataseries[i].weather === "rainsnowday" || data.dataseries[i].weather === "rainsnownight") {
+            img[i].src = "images/rainsnow.png";
+        }
+ 
+        else if(data.dataseries[i].weather === "tsday" || data.dataseries[i].weather === "tsnight") {
+            img[i].src = "images/tsrain.png";
+        }
+
+        else if(data.dataseries[i].weather === "tsrainday" || data.dataseries[i].weather === "tsrainnight") {
+            img[i].src = "images/tstorm.png";
+        }
+        else {
+            img[i].src = "images/windy.png";
+        }   
+    }
 }
 
 checkWeather();
 
-const appear = document.querySelector(".appear");
-appear.style.display = "none";
-
-const selectElement = document.getElementById('drop-down');
-
-        
-selectElement.addEventListener('input', function(event) {
-     if(event.target.value !== 0) {
-        appear.style.display = "block"
-     }
-});
